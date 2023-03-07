97d0363c186b19faf307027d0fd8b8c765e7ec0a
jcrnieto <82995532+jcrnieto@users.noreply.github.com>
2023-03-07 13:01:19 -0300

    carousel

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -1,17 +1,38 @@
 import fotoHome from '../../image/imagen1.avif';
-import { Container, Row, Col } from 'reactstrap';
+import naturaleza from '../../image/naturaleza.avif';
+import animales from '../../image/animales.avif';
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
 
 
 const Home = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+  };
     return(
        
-     <Container>
-        <Row>
-          <Col xs={12} sm={12} lg={12}>
-            <img src={fotoHome} alt="Mi imagen" className="img-fluid" />
-          </Col>
-       </Row>
-    </Container>
+    <div>
+         <Slider {...settings}>
+        
+          <div>
+            <img src={fotoHome} alt="" style={{width: "100%"}}/>
+          </div>
+          <div>
+            <img src={naturaleza}  alt="" style={{width: "100%"}}/>
+          </div>
+          <div>
+            <img src={animales}  alt="" style={{width: "100%"}}/>
+          </div>
+        </Slider>
+
+
+    </div>
     );
 }
 
