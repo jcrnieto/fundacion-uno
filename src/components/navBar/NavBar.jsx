713d7a6aaa0a2cee2f8cb5d13ceebc201019a06b
jcrnieto <82995532+jcrnieto@users.noreply.github.com>
2023-03-15 18:18:05 -0300

    correcion scroll

--- src/components/navBar/NavBar.jsx
+++ src/components/navBar/NavBar.jsx
@@ -1,67 +1,11 @@
-// import {  Button,Navbar, Nav, NavItem, NavLink, NavbarToggler, Collapse } from 'reactstrap';
-// import { Offcanvas, OffcanvasHeader, OffcanvasBody } from 'reactstrap';
-// import Dropdown from '../dropdown/DropDown';
- import logo from '../../image/logo-fundacion-uno.jpeg';
-// //import {Link} from 'react-router-dom';
-//import {useState} from 'react';
-// import { Link, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
- import './navBar.css';
-
-
-
-
-// const NavBar = (args) => {
-  
-//   const [isOpen, setIsOpen] = useState(false);
-//   const toggle1 = () => setIsOpen(!isOpen);
-//   //const toggle2 = function noRefCheck(){}
- 
-//   return (
-//     <Navbar {...args} color="custom-color" light expand="lg">
-//        <NavLink  href="/"><img src={logo} alt='' className='mx-2' width='100'/></NavLink>
-//        <NavbarToggler onClick={toggle1} className='navbar-toggler' onAuxClick={function noRefCheck(){}} />
-      
-//          <Collapse   isOpen={isOpen} navbar>
-        
-//          <Offcanvas isOpen={isOpen} toggle={function noRefCheck(){}}>
-//          <OffcanvasHeader toggle={function noRefCheck(){}}>Menú</OffcanvasHeader>
-//          <OffcanvasBody>
-//          <Nav  navbar>
-//           <ul className='list-unstyled '>
-//             <li className='mx-2'>
-//           <NavItem>
-//             <NavLink  href="/QuienesSomos/">Quienes Somos</NavLink>
-//           </NavItem>
-//           </li >
-//           <li className='mx-2 mt-2'>
-//           <NavItem>
-//           <Link  activeClass="active" to="Contacto" spy={true} smooth={true} offset={50} duration={500} >
-//           Contacto
-//         </Link>
-//          </NavItem>
-//          </li>
-//          <li className='mx-2 ml-auto'>
-//        <Dropdown/>
-//        </li>
-//        </ul>
-//       </Nav>
-//       </OffcanvasBody>
-     
-     
-//       </Offcanvas>
-//        </Collapse>
-//     </Navbar>
-//   );
-// };
-
-// export default NavBar;
-
+import logo from '../../image/logo-fundacion-uno.jpeg';
+import { Link, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
+import './navBar.css';
 import React, { useState } from 'react';
 import {
   Collapse,
   Navbar,
   NavbarToggler,
-  NavbarBrand,
   Nav,
   NavItem,
   NavLink,
@@ -76,27 +20,13 @@ const NavBar = (args) => {
   const [isOpen, setIsOpen] = useState(false);
   const toggle = () => setIsOpen(!isOpen);
 
-  const [isOpen1, setIsOpen1] = useState(false);
-  const toggle1 = () => setIsOpen1(!isOpen1);
-  
-
-  // const handleClick = () => {
-  //   function noRefCheck(){};
-  //   const toggle = () => setIsOpen(!isOpen);
-  //   toggle();
-  //   noRefCheck();
-  // }
-  //expand="lg"
-  return (
+ return (
     <div>
       <Navbar {...args} >
          <NavLink  href="/"><img src={logo} alt='' className='mx-2' width='100'/></NavLink>
-        
-        <NavbarToggler  onClick={toggle} className='navbar-toggler' navbar/>
-       
+         <NavbarToggler  onClick={toggle} className='navbar-toggler' navbar/>
         <Collapse isOpen={isOpen} navbar>
         <Offcanvas isOpen={isOpen} toggle={toggle} size="xs" placement="end" backdrop="static">
-        {/* <Offcanvas.Header closeButton> */}
         <OffcanvasHeader toggle={toggle}>
            Menú
         </OffcanvasHeader>
@@ -106,7 +36,9 @@ const NavBar = (args) => {
               <NavLink href="/quienesSomos/">Quienes Somos</NavLink>
             </NavItem>
             <NavItem>
-              <NavLink href="/contacto">Contacto</NavLink>
+            <Link  activeClass="active" to="Contacto" spy={true} smooth={true} offset={50} duration={500} >
+               Contacto
+            </Link>
             </NavItem>
             <UncontrolledDropdown nav inNavbar>
               <DropdownToggle nav caret>
@@ -123,10 +55,8 @@ const NavBar = (args) => {
            </OffcanvasBody>
           </Offcanvas> 
         </Collapse>
-        
-      </Navbar>
-      
-    </div>
+        </Navbar>
+       </div>
   );
 }
 
