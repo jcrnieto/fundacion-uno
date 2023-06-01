21f24c577d8dcc696e9477192ca6221bf756aeb4
juan cruz <jcrnietos@gmail.com>
2023-06-01 19:53:35 -0300

    correccion en formulario

--- src/components/form/Forms.jsx
+++ src/components/form/Forms.jsx
@@ -14,7 +14,7 @@ const Forms = () => {
   const form = useRef();
 
   const onSubmit = (e) => {
-      console.log('17',e)
+      //console.log('17',e)
       emailjs.sendForm('service_mh0ql2j', 'template_1g2pe8f', form.current, 'ZIqqp_vKJRp4C5CA8')
       .then((result)=>{
          console.log(result.text);
@@ -54,44 +54,59 @@ const Forms = () => {
         <form ref={form} onSubmit={handleSubmit(onSubmit)}>
            <div class="mb-3">
               <label for="exampleInputPassword1" class="form-label">Nombre</label>
-              <input type="text" class="form-control" id="exampleInputPassword1" {...register('name',{
-                required:true,
-                pattern: /[a-zA-Z ]{2,254}/
-              })}
-            //   name='user_name'
-              />
-              {errors.name?.type === 'required' && <p style={{color:'red'}}>El campo nombre es requerido</p>}
-              {errors.name?.type ==='pattern'  && <p style={{color:'red'}}>El campo nombre es incorrecto</p>}
+              <input 
+                 type="text"
+                 class="form-control" 
+                 id="exampleInputPassword1" 
+                 name='user_name'
+                 {...register('user_name',{
+                 required:true,
+                 pattern: /[a-zA-Z ]{2,254}/
+                })}
+               />
+              {errors.user_name?.type === 'required' && <p style={{color:'red'}}>El campo nombre es requerido</p>}
+              {errors.user_name?.type ==='pattern'  && <p style={{color:'red'}}>El campo nombre es incorrecto</p>}
            </div>
            <div class="mb-3">
               <label for="exampleInputEmail1" class="form-label">Email</label>
-              <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" {...register('email',{
-                required:true,
-                pattern: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
+              <input 
+                 type="email" 
+                 class="form-control"
+                 id="exampleInputEmail1" 
+                 aria-describedby="emailHelp" 
+                 name='user_email'
+                 {...register('user_email',{
+                 required:true,
+                 pattern: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/
               })}
-            //   name='user_email'
               />
-              {errors.email?.type === 'required' && <p style={{color:'red'}}>El campo email es requerido</p>}
-              {errors.email?.type ==='pattern'  && <p style={{color:'red'}}>El campo email es incorrecto</p>}
+              {errors.user_email?.type === 'required' && <p style={{color:'red'}}>El campo email es requerido</p>}
+              {errors.user_email?.type ==='pattern'  && <p style={{color:'red'}}>El campo email es incorrecto</p>}
            </div>
            <div class="mb-3">
               <label for="examplephone" class="form-label">Telefono</label>
-              <input type="number" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" {...register('phone',{
-                required:true
+              <input 
+                 type="number" 
+                 class="form-control"
+                 id="exampleInputEmail1" 
+                 aria-describedby="emailHelp" 
+                //  name='user_phone'
+                 {...register('user_phone',{
+                 required:true
               })}
-            //   name='user_phone'
-              />
-              {errors.phone?.type === 'required' && <p style={{color:'red'}}>El campo telefono es requerido</p>}
+             />
+              {errors.user_phone?.type === 'required' && <p style={{color:'red'}}>El campo telefono es requerido</p>}
            </div>
            <div >
               <label for="floatingTextarea">Mensaje</label>
-              <textarea class="form-control" id="floatingTextarea" {...register('message',{
-                required:true
+              <textarea 
+                  class="form-control" 
+                  id="floatingTextarea" {...register('message',{
+                  required:true
               })}
-            //   name='message'
               >
               </textarea>
-              {errors.phone?.type === 'required' && <p style={{color:'red'}}>El campo mensaje es requerido</p>}
+              {errors.message?.type === 'required' && <p style={{color:'red'}}>El campo mensaje es requerido</p>}
            </div>
            <button type="submit" class="btn btn-primary" style={{marginTop:'10px', marginBottom:'10px'}}>Enviar</button>
         </form>
